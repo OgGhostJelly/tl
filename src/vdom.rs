98b97b7efc56433c9662647b43e0bf2e372aa06f
OgGhostJelly <johnnycambodia09@gmail.com>
2025-03-14 10:19:04 +0700

    fix: VDomGuard::get_mut_ref not returning a mutable ref

--- src/vdom.rs
+++ src/vdom.rs
@@ -232,7 +232,7 @@ impl VDomGuard {
     /// Returns a mutable reference to the inner DOM.
     ///
     /// The lifetime of the returned `VDom` is bound to self so that elements cannot outlive this `VDomGuard` struct.
-    pub fn get_mut_ref<'a, 'b: 'a>(&'b mut self) -> &'b VDom<'a> {
+    pub fn get_mut_ref<'a, 'b: 'a>(&'b mut self) -> &'b mut VDom<'a> {
         &mut self.dom
     }
 }
